5a5ae99ccf272522890e4df091f4ff9011fdcd1e
Ignas <15948796+ignasc@users.noreply.github.com>
2025-07-10 15:21:21 +0100

    Add new line and indentation tests to eslint.

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ A generic webpack template for html, css and javascript.
 
 ### Important notes
 
-Make sure to modify the command line of 'deploy' script to refer to correct github branch used for production.
+Make sure to modify the command line of `"deploy"` script to refer to correct github branch used for production.
+
+### npm scripts
+
+`"test"`: runs `eslint` check on all files in `src` folder
+
+`"fix"`: runs `eslint` with `--fix` option.
+
+`"prebuild"`: runs `eslint` check automatically before building production version app
+
+`"build"`: builds production version app
+
+`"start"`: starts a dev server with the developer version app
+
+`"deploy"`: pushes gh-pages branch to github. Branch must have changes already commited.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,15 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
+const ruleNewLine = {"@stylistic/eol-last": ["warn", "always"]};
+const ruleIndent = {"@stylistic/indent": ["warn", 4],};
+
+const commonRules =  {...ruleNewLine, ...ruleIndent};
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, "@stylistic": stylistic }, extends: ["js/recommended"], rules: { ...commonRules } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
